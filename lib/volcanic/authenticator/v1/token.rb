f471a45012c94dc0ae1c818131e0671f3459ab62
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-04-26 09:51:45 +0800

    Add function get 'jti'

--- lib/volcanic/authenticator/v1/token.rb
+++ lib/volcanic/authenticator/v1/token.rb
@@ -1,31 +1,24 @@
 require 'base64'
-require 'jwt'
 
 module Volcanic
   module Authenticator
     # Helper for Token handling
     module Token
       def expiry_time(token)
-        JSON.parse(decode(token))['exp']
-        # validate_expiry(exp)
+        perform_get_claim token, 'exp'
       end
 
-      def self.decrypt(token)
-        JWT.decode token, public_key, true, algorithm: 'ES512'
-      end
-
-      def decode(token)
-        tokens = token.split('.')
-        return nil unless tokens.length == 3
-        return nil if tokens[1].nil? || tokens[1].empty?
-
-        Base64.decode64(tokens[1])
+      def jti(token)
+        perform_get_claim token, 'jti'
       end
 
       private
 
-      def public_key
-        "-----BEGIN PUBLIC KEY-----\nMIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQARs3UaQ82aORFh4BMUstcXHV85KT+\n6ZgAiWswUiGiAPvvrt4DPBj1MM81uSt46AKLllybGgjDbmxsxKR6GJxZZSsAbhRo\n351VCzTAc3IrzYpXAkvwpH5xpN0HEUFqYfdB54fz5EsV9Ib97lNCKceGUJH4fotS\nPnFgCjz0Z094WOKNimI=\n-----END PUBLIC KEY-----"
+      def perform_get_claim(token, object)
+        decoded = decode token
+        return nil if decoded.nil?
+
+        JSON.parse(decoded)[object]
       end
 
       def validate_expiry(exp)
@@ -36,6 +29,18 @@ module Volcanic
         return token_exp if token_exp < default_exp
 
         default_exp
+      end
+
+      def decode(token)
+        tokens = token.split('.')
+        return nil unless tokens.length == 3
+        return nil if tokens[1].nil? || tokens[1].empty?
+
+        begin
+          Base64.decode64(tokens[1])
+        rescue ArgumentError
+          nil
+        end
       end
     end
   end
