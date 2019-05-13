11cb5474ffaf9e69944b627fd11864928a5eb3ea
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-13 13:55:18 +0800

    AUTH-61 remove unuse method (merge from v1)

--- lib/volcanic/authenticator/v1/header.rb
+++ lib/volcanic/authenticator/v1/header.rb
@@ -7,24 +7,6 @@ module Volcanic
           { "Authorization": "Bearer #{token}",
             "Content-Type": 'application/json' }
         end
-
-        # private
-
-        # def auth_token(token)
-        #   token || local_token
-        # end
-        #
-        # def local_token
-        #   token = Cache.new.mtoken
-        #   return token unless token.nil?
-        #
-        #   perform_get_token
-        # end
-        #
-        # def perform_get_token
-        #   res = Connection.new.main_token
-        #   JSON.parse(res)['token']
-        # end
       end
     end
   end
