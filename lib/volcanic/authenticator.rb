69b57aa771c06c198127a81947265e24151b34e1
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-13 12:33:28 +0800

    AUTH-61 create config class callback and remove cache callback (merge from v1)

--- lib/volcanic/authenticator.rb
+++ lib/volcanic/authenticator.rb
@@ -6,11 +6,7 @@ module Volcanic
   module Authenticator
     module Base
       def config
-        Thread.current[:volcanic_authenticator_config] ||= Volcanic::Authenticator::Config.new
-      end
-
-      def cache
-        Thread.current[:volcanic_authenticator_cache2] ||= Volcanic::Cache::Cache.new
+        Volcanic::Authenticator::Config
       end
     end
 
