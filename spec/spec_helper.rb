630322cab54fcef19154bf0d7b5172cf1547d20a
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-15 11:54:35 +0800

    AUTH-61 add volcanic-cache path to rspec (merge from v1)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'bundler/setup'
 require 'volcanic/authenticator'
+require 'volcanic/cache'
+require 'volcanic/authenticator/v1/response_parser'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
