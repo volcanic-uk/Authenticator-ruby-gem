f9b0bfc31df1c0d8e0e6f37c0550170bf0bdacbb
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-21 12:07:06 +0800

    AUTH-61 add default expire time

--- lib/volcanic/authenticator/config.rb
+++ lib/volcanic/authenticator/config.rb
@@ -7,30 +7,42 @@ module Volcanic
       # singleton
       class << self
         attr_accessor :auth_url, :app_name, :app_secret
-        attr_reader :exp_token, :exp_app_token, :exp_public_key
+        # attr_reader :exp_token, :exp_app_token, :exp_public_key
+
+        def exp_token
+          @expire_token ||= 5 * 60
+        end
+
+        def exp_app_token
+          @expire_app_token ||= 24 * 60 * 60
+        end
+
+        def exp_public_key
+          @expire_public_key ||= 24 * 60 * 60
+        end
 
         def exp_token=(value)
-          return unless number? value
+          raise ExpTokenError unless integer? value
 
-          @exp_token = value.to_i
+          @expire_token = value.to_i
         end
 
         def exp_app_token=(value)
-          return unless number? value
+          raise ExpAppTokenError unless integer? value
 
-          @exp_app_token = value.to_i
+          @expire_app_token = value.to_i
         end
 
         def exp_public_key=(value)
-          return unless number? value
+          raise ExpPublicKeyError unless integer? value
 
-          @exp_public_key = value.to_i
+          @expire_public_key = value.to_i
         end
 
         private
 
-        def number?(obj)
-          obj.to_s == obj.to_i.to_s
+        def integer?(value)
+          value.is_a? Integer
         end
       end
     end
