0b641fa0c421a96df9f5917ef3c22dd18327fc75
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-23 13:08:14 +0800

    AUTH-92 add method to principal

--- lib/volcanic/authenticator/v1/principal.rb
+++ lib/volcanic/authenticator/v1/principal.rb
@@ -1,5 +1,51 @@
+require 'httparty'
+require 'forwardable'
+require_relative 'response'
+require_relative 'header'
+require_relative 'token'
+
 module Volcanic::Authenticator
   module V1
-    class Principal; end
+    class Principal
+      include HTTParty
+      extend SingleForwardable
+
+      # URLS
+      CREATE_URL = ''
+      UPDATE_URL = ''
+      DELETE_URL = ''
+
+      @_singleton_mutex = Mutex.new
+
+      class << self
+        def instance
+          @_singleton_mutex.synchronize { @_singleton_instance ||= new }
+        end
+      end
+
+      def initialize
+        self.class.base_uri Volcanic::Authenticator.config.auth_url
+      end
+
+      def create; end
+
+      def update; end
+
+      def delete; end
+
+      private
+
+      def request_create_principal
+        self.class.post(CREATE_URL)
+      end
+
+      def request_update_principal
+        self.class.post(UPDATE_URL)
+      end
+
+      def request_delete_principal
+        self.class.delete(DELETE_URL)
+      end
+    end
   end
 end
