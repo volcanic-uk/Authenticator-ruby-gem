6851f6af300f1f304e15a2a0d44847de0f7845d9
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-27 10:36:30 +0800

    AUTH-61 add base class. Handle similar method between classes

---
+++ lib/volcanic/authenticator/v1/base.rb
@@ -0,0 +1,13 @@
+require 'forwardable'
+
+module Volcanic::Authenticator
+  module V1
+    ##
+    # For now helper for Identity and TokenKey
+    class Base
+      extend SingleForwardable
+      def_delegator 'Volcanic::Cache::Cache'.to_sym, :instance, :cache
+      def_delegators 'Volcanic::Authenticator.config'.to_sym, :exp_app_token, :exp_public_key
+    end
+  end
+end
