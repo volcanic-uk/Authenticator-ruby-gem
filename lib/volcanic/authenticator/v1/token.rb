1c48310669caaeed69df6444dbc92bba99f4b5f0
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-27 10:34:07 +0800

    AUTH-61 update token class

--- lib/volcanic/authenticator/v1/token.rb
+++ lib/volcanic/authenticator/v1/token.rb
@@ -1,21 +1,26 @@
 require 'jwt'
+require 'httparty'
+require_relative 'base'
 
 module Volcanic::Authenticator
   module V1
     # Helper for Token handling
     class Token
-      def initialize(token, pkey)
-        @dec_token = decode token, pkey
+      extend Forwardable
+      def_instance_delegator 'Volcanic::Cache::Cache'.to_sym, :instance, :cache
+      def_instance_delegator 'Volcanic::Authenticator.config'.to_sym, :exp_token
+      attr_reader :token
+
+      def initialize(token)
+        @token = token
       end
 
-      def jti
-        @dec_token[0]['jti']
+      def caching!
+        cache.fetch token, expire_in: exp_token, &method(:token)
       end
 
-      private
-
-      def decode(token, pkey)
-        JWT.decode token, pkey, true, algorithm: 'ES512'
+      def valid?
+        JWT.decode token, TokenKey.fetch_and_request_public_key, true, algorithm: 'ES512'
       rescue JWT::DecodeError
         raise TokenError
       end
