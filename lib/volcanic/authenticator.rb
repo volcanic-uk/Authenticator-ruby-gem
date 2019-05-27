4e3841a894c163e594b1f7dbd07de2c30571d8c2
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-27 10:33:00 +0800

    AUTH-61 add constant urls

--- lib/volcanic/authenticator.rb
+++ lib/volcanic/authenticator.rb
@@ -7,6 +7,14 @@ require_relative 'authenticator/v1/principal'
 module Volcanic
   # Authenticator
   module Authenticator
+    PUBLIC_KEY = 'volcanic_public_key'.freeze
+    APP_TOKEN = 'volcanic_application_token'.freeze
+    IDENTITY_REGISTER = 'api/v1/identity'.freeze
+    IDENTITY_LOGIN = 'api/v1/identity/login'.freeze
+    IDENTITY_LOGOUT = 'api/v1/identity/logout'.freeze
+    IDENTITY_DEACTIVATE = 'api/v1/identity/deactivate'.freeze
+    IDENTITY_VALIDATE = 'api/v1/identity/validate'.freeze
+    PUBLIC_KEY_GENERATE = 'api/v1/key/public'.freeze
     ##
     # config helper
     module Base
