20bdf087666faa3fd7e5011d65a83d34cae87c23
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-05-31 11:21:31 +0800

    AUTH-92 update method

--- lib/volcanic/authenticator/v1/token.rb
+++ lib/volcanic/authenticator/v1/token.rb
@@ -15,21 +15,54 @@ module Volcanic::Authenticator
         @token = token
       end
 
-      def caching!
+      ##
+      # Cache token
+      # Eg.
+      #  Token.new('TOKEN_KEY').cache!
+      #
+      def cache!
         cache.fetch token, expire_in: exp_token, &method(:token)
       end
 
-      def decode!
-        JWT.decode token, TokenKey.fetch_and_request_public_key, true, algorithm: 'ES512'
-      rescue JWT::DecodeError
-        raise TokenError
-      end
-
+      ##
+      # Decode and get claims.
+      # Eg.
+      #  token.decode_with_claims!
+      #  token.kid # => key id claim
+      #  token.sub # => subject claim
+      #  token.iss # => issuer claim
+      #
+      #  token.dataset_id # => dataset id from sub
+      #  token.principal_id # => principal id from sub
+      #  token.identity_id # => identity id from su
+      #
       def decode_with_claims!
         fetch_claims(decode!)
       end
 
+      ##
+      # Verify token signature.
+      # Raise exception (TokenError) for invalid token.
+      # Eg.
+      #  token.verify!
+      #
+      def verify!
+        ##
+        # Decode and get claims. Basically this is to retrieve KID
+        # fetch_claims(decode!)
+        ##
+        # Decode with verify signature
+        # decode!(TokenKey.fetch_and_request_public_key(kid), true)
+        decode!(TokenKey.fetch_and_request_public_key, true)
+      end
+
       private
+
+      def decode!(public_key = nil, verify = false)
+        JWT.decode token, public_key, verify, algorithm: 'ES512'
+      rescue JWT::DecodeError => e
+        raise TokenError, e
+      end
 
       def fetch_claims(dec_token)
         header = dec_token.last
