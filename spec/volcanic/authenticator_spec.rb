04e278f9aebdc0c622bc7a34d95e017a4f80e256
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-18 18:09:42 +0800

    1) add configuration class 2) add configuration spec 3) add exception class

--- spec/volcanic/authenticator_spec.rb
+++ spec/volcanic/authenticator_spec.rb
@@ -1,9 +1,20 @@
 RSpec.describe Volcanic::Authenticator do
-  it "has a version number" do
-    expect(Volcanic::Authenticator::VERSION).not_to be nil
-  end
+  describe 'Config' do
+    let(:config) { Volcanic::Authenticator.config }
+    context 'When missing auth url' do
+      it { expect(config.auth_url).to be nil }
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    context 'When missing application name' do
+      it { expect(config.app_name).to be nil }
+    end
+
+    context 'When missing application secret' do
+      it { expect(config.app_secret).to be nil }
+    end
+
+    context 'When invalid expiration value' do
+      it { expect { config.exp_token = '123' }.to raise_error Volcanic::Authenticator::V1::ConfigurationError }
+    end
   end
 end
