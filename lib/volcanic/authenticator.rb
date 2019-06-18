30eee2e508856a6f16fe87620167b9cbd751f092
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-18 18:09:42 +0800

    1) add configuration class 2) add configuration spec 3) add exception class

--- lib/volcanic/authenticator.rb
+++ lib/volcanic/authenticator.rb
@@ -1,5 +1,17 @@
+require_relative '../volcanic.rb'
+require_relative 'authenticator/v1/config.rb'
+require_relative 'authenticator/v1/exception.rb'
+
 module Volcanic
   # Authenticator
   module Authenticator
+    # Authenticator base
+    module Base
+      def config
+        Volcanic::Authenticator::V1::Config
+      end
+    end
+
+    extend Base
   end
 end
---
+++ lib/volcanic/authenticator/v1/config.rb
@@ -0,0 +1,61 @@
+module Volcanic::Authenticator
+  module V1
+    ##
+    # Configuration class
+    #
+    # after installation of this gem, these need to be configure.
+    class Config
+      class << self
+        attr_accessor :auth_url, :app_name, :app_secret
+        attr_writer :key_store_type
+
+        # currently auth service has 2 types of key_store
+        # Dynamic and Static. Generally this is regarding to key rotation.
+        # After the key rotation features is finalise, this will be remove.
+        # Only dynamic key will be use.
+        def key_store_type
+          @key_store_type ||= 'static'
+        end
+
+        # expiration time of cache tokens
+        def exp_token
+          @exp_token ||= 5 * 60 # default of 5 minutes
+        end
+
+        # expiration time of cache application token
+        def exp_app_token
+          @exp_app_token ||= 24 * 60 * 60 # default of 1 day
+        end
+
+        # expiration time of cache public keys
+        def exp_public_key
+          @exp_public_key ||= 24 * 60 * 60 # default of 1 day
+        end
+
+        def exp_token=(value)
+          raise ConfigurationError unless integer? value
+
+          @exp_token = value.to_i
+        end
+
+        def exp_app_token=(value)
+          raise ConfigurationError unless integer? value
+
+          @exp_app_token = value.to_i
+        end
+
+        def exp_public_key=(value)
+          raise ConfigurationError unless integer? value
+
+          @exp_public_key = value.to_i
+        end
+
+        private
+
+        def integer?(value)
+          value.is_a? Integer
+        end
+      end
+    end
+  end
+end
