948db4f617b7560b2c73e9fd1286e4ed6761089e
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-19 10:26:59 +0800

    update exception with error class

--- lib/volcanic/authenticator/v1/exception.rb
+++ lib/volcanic/authenticator/v1/exception.rb
@@ -1,6 +1,12 @@
 module Volcanic::Authenticator
   module V1
     class AuthenticatorError < StandardError; end
+    ##
     class ConfigurationError < AuthenticatorError; end
+    class ApplicationTokenError < AuthenticatorError; end
+    ##
+    class ConnectionError < AuthenticatorError; end
+    class AuthorisationError < AuthenticatorError; end
+    class SignatureError < AuthenticatorError; end
   end
 end
