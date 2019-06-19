2f8d5a5a1c251aed77713f912d05e29803ab84a5
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-19 17:34:15 +0800

    helper-method 1) use request helper 2) simplify code

--- lib/volcanic/authenticator/v1/helper/app_token.rb
+++ lib/volcanic/authenticator/v1/helper/app_token.rb
@@ -4,6 +4,7 @@ require 'httparty'
 require_relative 'header'
 require_relative 'error'
 require_relative '../base'
+require_relative 'request'
 
 module Volcanic::Authenticator
   module V1
@@ -16,8 +17,11 @@ module Volcanic::Authenticator
       class << self
         include Error
         include Header
+        include Request
 
         APP_TOKEN = 'volcanic_application_token'
+        # Token end-point
+        GENERATE_TOKEN_URL = 'api/v1/identity/login'
 
         def fetch_and_request
           cache.fetch APP_TOKEN, expire_in: exp_app_token, &method(:request_app_token)
@@ -29,12 +33,10 @@ module Volcanic::Authenticator
           name = Volcanic::Authenticator.config.app_name
           secret = Volcanic::Authenticator.config.app_secret
           payload = { name: name, secret: secret }.to_json
-          res = HTTParty.post(url, body: payload)
+          res = perform_post_request url, payload, nil
           raise_exception_app_token(res) unless res.success?
           # return token string
           JSON.parse(res.body)['response']['token']
-        rescue Timeout::Error, Errno::EINVAL, Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError => e
-          raise ConnectionError, e
         end
       end
     end
