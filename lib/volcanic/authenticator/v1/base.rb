32c1c1d28b33c29c7ea8d2a85311926c92f81281
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-19 10:26:37 +0800

    add base class for authenticator. this contains Volcanic::Cache and volcanic::Authenticator.config forwardable

---
+++ lib/volcanic/authenticator/v1/base.rb
@@ -0,0 +1,15 @@
+require 'forwardable'
+
+module Volcanic::Authenticator
+  module V1
+    ##
+    # Base class
+    # Contains of Volcanic::Cache::Cache and
+    # Volcanic::Authenticator.config forwardable.
+    class Base
+      extend SingleForwardable
+      def_delegator 'Volcanic::Cache::Cache'.to_sym, :instance, :cache
+      def_delegators 'Volcanic::Authenticator.config'.to_sym, :exp_app_token, :exp_public_key, :app_issuer
+    end
+  end
+end
