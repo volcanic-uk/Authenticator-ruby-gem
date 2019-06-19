b9d9b742dd443f7c3eb9a53fb15721019236f248
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-19 17:36:07 +0800

    helper-method 1) remove constants to its specific class

--- lib/volcanic/authenticator.rb
+++ lib/volcanic/authenticator.rb
@@ -7,11 +7,6 @@ require_relative 'authenticator/v1/exception.rb'
 module Volcanic
   # Authenticator
   module Authenticator
-    # Token end-point
-    GENERATE_TOKEN_URL = 'api/v1/identity/login'
-    # Public Key end-point
-    PUBLIC_KEY_URL = 'api/v1/key'
-
     # Authenticator base
     module Base
       def config
