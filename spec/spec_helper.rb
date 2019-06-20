5de9158b91591234c54e19900f0506457aba3bc7
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-20 23:19:39 +0800

    add application token and public key rspec (test). add vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 require 'rspec/its'
 require 'volcanic/authenticator'
 require 'volcanic/cache'
+require 'vcr'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -14,5 +15,27 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+VCR.configure do |c|
+  c.configure_rspec_metadata!
+  c.cassette_library_dir = "spec/vcr"
+  c.hook_into :webmock
+end
+
+module Configuration
+  # configure setting
+  def self.set
+    Volcanic::Authenticator.config.auth_url = 'http://0.0.0.0:3003'
+    Volcanic::Authenticator.config.app_name = 'volcanic'
+    Volcanic::Authenticator.config.app_secret = 'volcanic!123'
+  end
+
+  # reset setting
+  def self.reset
+    Volcanic::Authenticator.config.auth_url = nil
+    Volcanic::Authenticator.config.app_name = nil
+    Volcanic::Authenticator.config.app_secret = nil
   end
 end
