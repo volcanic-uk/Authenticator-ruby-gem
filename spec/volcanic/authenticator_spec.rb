42e90eb45ac11c267c144c0d4a1c1cd45aff365d
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-20 23:19:39 +0800

    add application token and public key rspec (test). add vcr

--- spec/volcanic/authenticator_spec.rb
+++ spec/volcanic/authenticator_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Volcanic::Authenticator do
+RSpec.describe Volcanic::Authenticator, :vcr do
   describe 'Config' do
     let(:config) { Volcanic::Authenticator.config }
     context 'When missing auth url' do
@@ -15,8 +15,51 @@ RSpec.describe Volcanic::Authenticator do
       it { expect(config.app_secret).to be nil }
     end
 
-    context 'When invalid expiration value' do
+    context 'When invalid expiration token' do
       it { expect { config.exp_token = '123' }.to raise_error Volcanic::Authenticator::V1::ConfigurationError }
+    end
+
+    context 'When invalid expiration application token' do
+      it { expect { config.exp_app_token = '123' }.to raise_error Volcanic::Authenticator::V1::ConfigurationError }
+    end
+
+    context 'When invalid expiration public key' do
+      it { expect { config.exp_public_key = '123' }.to raise_error Volcanic::Authenticator::V1::ConfigurationError }
+    end
+  end
+
+  describe 'Helper' do
+    before { Configuration.set }
+    let(:cache) { Volcanic::Cache::Cache.instance }
+
+    describe 'Application Token' do
+      let(:app_token) { Volcanic::Authenticator::V1::AppToken }
+
+      context 'When requesting' do
+        subject { app_token.request_app_token }
+        it { should_not be nil }
+      end
+
+      context 'When fetch and requesting' do
+        subject { app_token.fetch_and_request }
+        it { should_not be nil }
+        it { expect(cache.fetch('volcanic_application_token')).not_to be nil }
+      end
+    end
+
+    describe 'Public Key' do
+      let(:key) { Volcanic::Authenticator::V1::Key }
+
+      context 'When requesting' do
+        subject { key.request_public_key }
+        it { should_not be nil }
+      end
+
+      context 'When fetch and requesting' do
+        subject { key.fetch_and_request }
+        it { should_not be nil }
+        it { expect(cache.fetch('volcanic_public_key')).not_to be nil }
+      end
     end
   end
 end
