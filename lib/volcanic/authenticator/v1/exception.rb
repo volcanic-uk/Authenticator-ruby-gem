f1df5d51aa84f5eb29968ad4c45237c74db2a844
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-20 17:17:21 +0800

    add token method

--- lib/volcanic/authenticator/v1/exception.rb
+++ lib/volcanic/authenticator/v1/exception.rb
@@ -2,5 +2,6 @@ module Volcanic::Authenticator
   module V1
     class AuthenticatorError < StandardError; end
     class ConfigurationError < AuthenticatorError; end
+    class TokenError < AuthenticatorError; end
   end
 end
