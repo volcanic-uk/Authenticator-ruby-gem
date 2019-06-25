7c24330465e1d9640542923b9728bf7124d62013
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-25 13:26:49 +0800

    Identity: add identity method and rspec

--- spec/volcanic/authenticator/v1/identity_spec.rb
+++ spec/volcanic/authenticator/v1/identity_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Volcanic::Authenticator::V1::Identity do
   before { Configure.set }
   let(:mock_name) { SecureRandom.hex(6) }
@@ -5,10 +7,8 @@ RSpec.describe Volcanic::Authenticator::V1::Identity do
   let(:principal) { Principal.create(mock_name, mock_dataset_id) }
 
   describe 'Create' do
-
   end
 
   describe 'Delete' do
-
   end
 end
