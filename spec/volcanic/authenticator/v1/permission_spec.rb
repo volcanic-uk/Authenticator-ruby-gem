0c706a5efb078df91e1ca2c8f323c93385868dff
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-26 11:51:19 +0800

    Auth-30 1. add permission rspec and vcr

--- spec/volcanic/authenticator/v1/permission_spec.rb
+++ spec/volcanic/authenticator/v1/permission_spec.rb
@@ -1,35 +1,27 @@
-RSpec.describe Volcanic::Authenticator::V1::Permission do
+# frozen_string_literal: true
+
+RSpec.describe Volcanic::Authenticator::V1::Permission, :vcr do
   before { Configuration.set }
   let(:permission) { Volcanic::Authenticator::V1::Permission }
   let(:mock_name) { SecureRandom.hex(6) }
   let(:mock_description) { 'mock_description' }
   describe 'Permission' do
-    subject(:new_permission) { permission.create(mock_name) }
+    subject(:new_permission) { permission.create(mock_name, mock_description) }
     describe 'Create' do
       context 'When missing name' do
-        it { expect { permission.create(nil) }.to raise_error Volcanic::Authenticator::PermissionError }
-        it { expect { permission.create('') }.to raise_error Volcanic::Authenticator::PermissionError }
+        it { expect { permission.create(nil, mock_description) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+        it { expect { permission.create('', mock_description) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
-      context 'When invalid/short name' do
-        it { expect { permission.create('shrt') }.to raise_error Volcanic::Authenticator::PermissionError }
-      end
-
-      context 'When duplicate name' do
-        let(:duplicate_name) { SecureRandom.hex 6 }
-        before { permission.create(duplicate_name) }
-        it { expect { permission.create(duplicate_name) }.to raise_error Volcanic::Authenticator::PermissionError }
+      context 'When missing description' do
+        it { expect { permission.create(mock_name, nil) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+        it { expect { permission.create(mock_name, '') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When success' do
         its(:name) { should_not be nil }
         its(:id) { should_not be nil }
         its(:creator_id) { should_not be nil }
-      end
-
-      context 'with description' do
-        subject { permission.create(mock_name, mock_description) }
-        its(:description) { should_not be nil }
       end
     end
 
@@ -40,12 +32,12 @@ RSpec.describe Volcanic::Authenticator::V1::Permission do
 
     describe 'Find by given id' do
       context 'When missing id' do
-        it { expect { permission.find_by_id(nil) }.to raise_error Volcanic::Authenticator::ConnectionError }
-        it { expect { permission.find_by_id('') }.to raise_error Volcanic::Authenticator::ConnectionError }
+        it { expect { permission.find_by_id(nil) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+        it { expect { permission.find_by_id('') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When invalid id' do
-        it { expect { permission.find_by_id('wrong_id') }.to raise_error Volcanic::Authenticator::PermissionError }
+        it { expect { permission.find_by_id('wrong_id') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When retrieve by id' do
@@ -60,32 +52,49 @@ RSpec.describe Volcanic::Authenticator::V1::Permission do
     describe 'Update' do
       let(:attr) { { name: SecureRandom.hex(6) } }
       context 'When missing id' do
-        it { expect { permission.update(nil, attr) }.to raise_error Volcanic::Authenticator::ConnectionError }
-        it { expect { permission.update('', attr) }.to raise_error Volcanic::Authenticator::ConnectionError }
+        it { expect { permission.update(nil, attr) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+        it { expect { permission.update('', attr) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When invalid id' do
-        it { expect { permission.update('wrong_id', attr) }.to raise_error Volcanic::Authenticator::PermissionError }
+        it { expect { permission.update('wrong_id', attr) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When invalid attributes' do
-        it { expect { permission.update(new_permission.id, wrong_attr: SecureRandom.hex(6)) }.to raise_error Volcanic::Authenticator::PermissionError }
+        it { expect { permission.update(new_permission.id, wrong_attr: SecureRandom.hex(6)) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+      end
+
+      context 'When attributes not a hash value' do
+        it { expect { permission.update(new_permission.id, 'not_hash') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When success' do
         subject { permission.update(new_permission.id, attr) }
         it { should_not be raise_error }
       end
+
+      describe 'attributes with descriptions' do
+        let(:has_descr) { { name: SecureRandom.hex(6), description: mock_description } }
+        let(:no_descr) { { name: SecureRandom.hex(6), description: nil } }
+        context 'When missing description value' do
+          it { expect { permission.update(new_permission.id, no_descr) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+        end
+        context 'When success' do
+          subject { permission.update(new_permission.id, has_descr) }
+          it { should_not be raise_error }
+        end
+
+      end
     end
 
     describe 'Delete' do
       context 'When missing id' do
-        it { expect { permission.delete(nil) }.to raise_error Volcanic::Authenticator::ConnectionError }
-        it { expect { permission.delete('') }.to raise_error Volcanic::Authenticator::ConnectionError }
+        it { expect { permission.delete(nil) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+        it { expect { permission.delete('') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When invalid id' do
-        it { expect { permission.delete('wrong_id') }.to raise_error Volcanic::Authenticator::PermissionError }
+        it { expect { permission.delete('wrong_id') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
       end
 
       context 'When success' do
@@ -95,4 +104,3 @@ RSpec.describe Volcanic::Authenticator::V1::Permission do
     end
   end
 end
-
