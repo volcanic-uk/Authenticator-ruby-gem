22c002aee0ee81647821d79d7578b769cfdbd278
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-06-26 12:23:15 +0800

    AUTH-33 merge and fix conflict

--- lib/volcanic/authenticator/v1/exception.rb
+++ lib/volcanic/authenticator/v1/exception.rb
@@ -10,5 +10,6 @@ module Volcanic::Authenticator
     class ConnectionError < AuthenticatorError; end
     class AuthorizationError < AuthenticatorError; end
     class SignatureError < AuthenticatorError; end
+    class GroupError < AuthenticatorError; end
   end
 end
