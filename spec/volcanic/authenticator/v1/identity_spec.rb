16f0d7b9fea68741ac07f5fb774dc9c46ae1a4fb
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-02 16:12:49 +0800

    rspec and vcr

--- spec/volcanic/authenticator/v1/identity_spec.rb
+++ spec/volcanic/authenticator/v1/identity_spec.rb
@@ -1,14 +1,62 @@
 # frozen_string_literal: true
 
-RSpec.describe Volcanic::Authenticator::V1::Identity do
-  before { Configure.set }
-  let(:mock_name) { SecureRandom.hex(6) }
-  let(:mock_dataset_id) { 1 }
-  let(:principal) { Principal.create(mock_name, mock_dataset_id) }
+require 'jwt'
 
+RSpec.describe Volcanic::Authenticator::V1::Identity, :vcr do
+  before { Configuration.set }
+  let(:mock_name) { SecureRandom.hex(6) }
+  let(:mock_secret) { SecureRandom.hex(6) }
+  let(:principal_id) { 1 } # need to create mock principal
+  let(:identity) { Volcanic::Authenticator::V1::Identity }
+  let(:identity_error) { Volcanic::Authenticator::V1::IdentityError }
   describe 'Create' do
+    context 'When missing name' do
+      it { expect { identity.register(nil, principal_id) }.to raise_error identity_error }
+      it { expect { identity.register('', principal_id) }.to raise_error identity_error }
+    end
+
+    context 'When duplicate name' do
+      let(:name) { SecureRandom.hex(6) }
+      before { identity.register(name, principal_id) }
+      it { expect { identity.register(name, principal_id) }.to raise_error identity_error }
+    end
+
+    context 'When missing principal id' do
+      it { expect { identity.register(mock_name, nil) }.to raise_error identity_error }
+      it { expect { identity.register(mock_name, '') }.to raise_error identity_error }
+    end
+
+    context 'When invalid principal id' do
+      it { expect { identity.register(mock_name, 'wrong-id') }.to raise_error identity_error }
+    end
+
+    context 'When success' do
+      subject { identity.register(mock_name, principal_id) }
+      it { should be_an identity }
+    end
+
+    context 'When missing secret' do
+      subject { identity.register(mock_name, principal_id, nil) }
+      its(:secret) { should_not be nil }
+    end
+
+    context 'When given secret' do
+      subject { identity.register(mock_name, principal_id, mock_secret) }
+      its(:secret) { should be mock_secret }
+    end
+
+    context 'When request a token with nil secret' do
+      subject { identity.register(mock_name, principal_id) }
+      its(:token) { should_not be nil }
+    end
+
+    context 'When request a token with secret' do
+      subject { identity.register(mock_name, principal_id, mock_secret) }
+      its(:token) { should_not be nil }
+    end
   end
 
   describe 'Delete' do
+    it { expect(identity.register(mock_name, principal_id).deactivate).not_to be raise_error }
   end
 end
--- spec/volcanic/authenticator/v1/token_spec.rb
+++ spec/volcanic/authenticator/v1/token_spec.rb
@@ -1,69 +1,70 @@
 # frozen_string_literal: true
 
-RSpec.describe Volcanic::Authenticator::V1::Token do
-  before { Configuration.set }
+RSpec.describe Volcanic::Authenticator::V1::Token, :vcr do
   let(:mock_name) { SecureRandom.hex(6) }
+  let(:identity) { Volcanic::Authenticator::V1::Identity.register(mock_name, 1) }
   let(:token) { Volcanic::Authenticator::V1::Token }
-  let(:identity) { Volcanic::Authenticator::V1::Identity.create(mock_name) }
-  subject(:new_token) { token.create(identity.name, identity.secret) }
-  describe 'Creating' do
+  let(:identity_error) { Volcanic::Authenticator::V1::IdentityError }
+  let(:authorization_error) { Volcanic::Authenticator::V1::AuthorizationError }
+  let(:token_error) { Volcanic::Authenticator::V1::TokenError }
+  subject(:new_token) { Volcanic::Authenticator::V1::Token.create(identity.name, identity.secret) }
+
+  describe 'Create' do
     context 'When name is missing' do
-      it { expect { token.create(nil, identity.secret) }.to raise_error Volcanic::Authenticator::TokenError }
-      it { expect { token.create('', identity.secret) }.to raise_error Volcanic::Authenticator::TokenError }
+      it { expect { token.create(nil, identity.secret) }.to raise_error identity_error }
+      it { expect { token.create('', identity.secret) }.to raise_error identity_error }
     end
 
     context 'When secret is missing' do
-      it { expect { token.create(identity.name, nil) }.to raise_error Volcanic::Authenticator::TokenError }
-      it { expect { token.create(identity.name, '') }.to raise_error Volcanic::Authenticator::TokenError }
+      it { expect { token.create(identity.name, nil) }.to raise_error identity_error }
+      it { expect { token.create(identity.name, '') }.to raise_error identity_error }
     end
 
     context 'When invalid name or secret' do
-      it { expect { token.create('wrong-name', identity.secret) }.to raise_error Volcanic::Authenticator::TokenError }
-      it { expect { token.create(identity.name, 'wrong-secret') }.to raise_error Volcanic::Authenticator::TokenError }
+      it { expect { token.create('wrong-name', identity.secret) }.to raise_error authorization_error }
+      it { expect { token.create(identity.name, 'wrong-secret') }.to raise_error authorization_error }
     end
 
     context 'When success' do
-      it { is_expected.to be_an identity }
-      it(:token) { should_not be nil }
+      its(:token_key) { should_not be nil }
     end
-  end
-
-  describe 'Cache a token' do
-    subject { new_token.cache! }
-    it { should_not be raise_error }
   end
 
   describe 'Validate' do
-    subject { new_token.validate }
-    context 'When token is invalid' do
-      it { expect { token.new(nil).validate }.to be false }
-      it { expect { token.new('').validate }.to be false }
-      it { expect { token.new('wrong-token').validate }.to be false }
+    context 'When missing token' do
+      it { expect(token.new(nil).validate).to be false }
+      it { expect(token.new('').validate).to be false }
+    end
+
+    context 'When invalid token' do
+      it { expect(token.new('wrong-token').validate).to be false }
     end
 
     context 'When token is valid' do
-      it { should be true }
+      it { expect(token.new(new_token.token_key).validate).to be true }
     end
   end
 
   describe 'Validate by Auth service' do
-    subject { new_token.validate_by_service }
-    context 'When token is invalid' do
-      it { expect { token.new(nil).validate_by_service }.to be false }
-      it { expect { token.new('').validate_by_service }.to be false }
-      it { expect { token.new('wrong-token').validate_by_service }.to be false }
+    context 'When missing token' do
+      it { expect(token.new(nil).validate_by_service).to be false }
+      it { expect(token.new('').validate_by_service).to be false }
+    end
+
+    context 'When invalid token' do
+      it { expect(token.new('wrong-token').validate_by_service).to be false }
     end
 
     context 'When token is valid' do
-      it { should be true }
+      it { expect(token.new(new_token.token_key).validate_by_service).to be true }
     end
   end
 
-  describe 'Get claims' do
+  describe 'Claims' do
     subject { new_token.decode_and_fetch_claims }
 
     context 'When success' do
-      its(:token) { should_not be nil }
+      its(:token_key) { should_not be nil }
       its(:kid) { should_not be nil }
       its(:sub) { should_not be nil }
       its(:iss) { should_not be nil }
@@ -73,16 +74,19 @@ RSpec.describe Volcanic::Authenticator::V1::Token do
     end
   end
 
-  describe 'Revoke token' do
+  describe 'Revoke' do
     before { new_token.cache! }
     subject { new_token.revoke! }
     context 'When token is invalid' do
-      it { expect { token.new('').revoke! }.to raise_error Volcanic::Authenticator::TokenError }
-      it { expect { token.new(nil).revoke! }.to raise_error Volcanic::Authenticator::TokenError }
-      it { expect { token.new('wrong-token').revoke! }.to raise_error Volcanic::Authenticator::TokenError }
+      it { expect { token.new('').revoke! }.to raise_error authorization_error }
+      it { expect { token.new(nil).revoke! }.to raise_error authorization_error }
     end
 
-    context 'When success' do
+    context 'When invalid token' do
+      it { expect { token.new('wrong-token').revoke! }.to raise_error authorization_error }
+    end
+
+    context 'When revoked' do
       it { should_not be raise_error }
     end
   end
