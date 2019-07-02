ca253e5cee4d84b80d7aee5e8082f4c433ff4075
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-02 16:08:11 +0800

    Update identity and token class method

--- lib/volcanic/authenticator/v1/token.rb
+++ lib/volcanic/authenticator/v1/token.rb
@@ -21,10 +21,10 @@ module Volcanic::Authenticator
 
       def_instance_delegator 'Volcanic::Cache::Cache'.to_sym, :instance, :cache
       def_instance_delegators 'Volcanic::Authenticator.config'.to_sym, :exp_token, :key_store_type
-      attr_reader :token, :kid, :sub, :iss, :dataset_id, :principal_id, :identity_id
+      attr_reader :token_key, :kid, :sub, :iss, :dataset_id, :principal_id, :identity_id
 
-      def initialize(token)
-        @token = token
+      def initialize(token_key)
+        @token_key = token_key
       end
 
       #
@@ -32,14 +32,14 @@ module Volcanic::Authenticator
       # if token exist at cache it returns true.
       # if token has a valid signature it return true.
       # eg.
-      #   Token.new(token).validate
+      #   Token.new(token_key).validate
       #   # => true/false
       #
       def validate
-        return true if cache.key?(token)
+        return true if cache.key?(token_key)
 
-        verify_signature # verify signature
-        cache! # if success verify it cache the token again
+        verify_signature
+        cache! # if verify_signature do not raise TokenError it cache the token again
         true
       rescue TokenError
         false
@@ -49,11 +49,11 @@ module Volcanic::Authenticator
       # to validate token by authenticator service
       #
       # eg.
-      #   Token.new(token).validate_by_service
+      #   Token.new(token_key).validate_by_service
       #   # => true/false
       #
       def validate_by_service
-        payload = { token: token }.to_json
+        payload = { token: token_key }.to_json
         res = perform_post_request(VALIDATE_TOKEN_URL, payload, nil)
         res.success?
       end
@@ -62,38 +62,42 @@ module Volcanic::Authenticator
       # to decode and fetch claims.
       #
       # eg.
-      #  token = Token.new(token).decode_and_fetch_claims
+      #  token = Token.new(token_key).decode_and_fetch_claims
       #  token.kid # => key id claim
       #  token.sub # => subject claim
       #  token.iss # => issuer claim
       #
       #  token.dataset_id # => dataset id from sub
       #  token.principal_id # => principal id from sub
-      #  token.identity_id # => identity id from su
+      #  token.identity_id # => identity id from sub
       #
       def decode_and_fetch_claims
         fetch_claims
         self
       end
 
+      def cache!
+        cache.fetch token_key, expire_in: exp_token, &method(:token_key)
+      end
+
       #
       # to remove/revoke token from cache,
       # then blacklist the token at auth service.
       # eg.
-      #   Token.new(token).revoke!
+      #   Token.new(token_key).revoke!
       #
       def revoke!
-        cache.evict! token
-        res = perform_post_request(REVOKE_TOKEN_URL, nil, token)
-        raise_exception_token(res) unless res.success?
+        cache.evict! token_key
+        res = perform_post_request(REVOKE_TOKEN_URL, nil, token_key)
+        raise_exception_identity(res) unless res.success?
       end
 
       #
-      # to request/generate a new token
+      # to request/generate a new token key
       #
       # eg.
       #   token = Token.create(name, secret)
-      #   token.token #=> <GENERATED_TOKEN>
+      #   token.token_key #=> <GENERATED_TOKEN>
       #   ...
       #
       class << self
@@ -111,10 +115,6 @@ module Volcanic::Authenticator
 
       private
 
-      def cache!
-        cache.fetch token, expire_in: exp_token, &method(:token)
-      end
-
       def verify_signature
         # fetch claims to obtain KID
         fetch_claims
@@ -124,7 +124,7 @@ module Volcanic::Authenticator
       end
 
       def decode!(public_key = nil, verify = false)
-        JWT.decode token, public_key, verify, algorithm: 'ES512'
+        JWT.decode token_key, public_key, verify, algorithm: 'ES512'
       rescue JWT::DecodeError => e
         raise TokenError, e
       end
