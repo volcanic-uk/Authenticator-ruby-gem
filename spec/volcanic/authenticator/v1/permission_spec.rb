d0150601e0ba967f2827ea5246686ef54cd53168
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-05 07:26:05 +0800

    rspec and vcr

--- spec/volcanic/authenticator/v1/permission_spec.rb
+++ spec/volcanic/authenticator/v1/permission_spec.rb
@@ -3,41 +3,36 @@
 RSpec.describe Volcanic::Authenticator::V1::Permission, :vcr do
   before { Configuration.set }
   let(:permission) { Volcanic::Authenticator::V1::Permission }
-  let(:mock_name) { SecureRandom.hex(6) }
+  let(:mock_name) { 'mock_permission_name' }
+  let(:mock_description) { 'mock_permission_description' }
   let(:service_id) { 1 } # mock service id
-  let(:mock_description) { 'mock_description' }
+  let(:permission_error) { Volcanic::Authenticator::V1::PermissionError }
   let(:new_permission) { permission.create(mock_name, service_id, mock_description) }
 
   describe 'Create' do
-    subject { permission.create(mock_name, service_id, mock_description) }
     context 'When missing name' do
-      it { expect { permission.create(nil, service_id) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
-      it { expect { permission.create('', service_id) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+      it { expect { permission.create(nil, service_id) }.to raise_error permission_error }
+      it { expect { permission.create('', service_id) }.to raise_error permission_error }
     end
 
     context 'When missing service id' do
-      it { expect { permission.create(mock_name, nil) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
-      it { expect { permission.create(mock_name, '') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+      it { expect { permission.create(mock_name, nil) }.to raise_error permission_error }
+      it { expect { permission.create(mock_name, '') }.to raise_error permission_error }
     end
 
     context 'When duplicate name' do
-      let(:duplicate_name) { SecureRandom.hex(6) }
-      before { permission.create(duplicate_name, service_id, mock_description) }
-      it { expect { permission.create(duplicate_name, service_id, mock_description) }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+      before { permission.create(mock_name, service_id, mock_description) }
+      it { expect { permission.create(mock_name, service_id, mock_description) }.to raise_error permission_error }
     end
 
     context 'When success' do
-      its(:name) { should_not be nil }
-      its(:id) { should_not be nil }
-      its(:service_id) { should_not be nil }
-      its(:subject_id) { should_not be nil }
+      subject { new_permission }
+      its(:name) { should eq mock_name }
+      its(:id) { should eq 1 }
+      its(:service_id) { should eq service_id }
+      its(:subject_id) { should eq 2 }
       its(:description) { should eq mock_description }
     end
-  end
-
-  describe 'Get all' do
-    subject { permission.all }
-    it { is_expected.to all(be_an(permission)) }
   end
 
   describe 'Find by given id' do
@@ -47,10 +42,10 @@ RSpec.describe Volcanic::Authenticator::V1::Permission, :vcr do
     end
 
     context 'When invalid id' do
-      it { expect { permission.find_by_id('wrong_id') }.to raise_error Volcanic::Authenticator::V1::PermissionError }
+      it { expect { permission.find_by_id('wrong_id') }.to raise_error permission_error }
     end
 
-    context 'When retrieved' do
+    context 'When success' do
       subject { permission.find_by_id(new_permission.id) }
       its(:name) { should eq new_permission.name }
       its(:id) { should eq new_permission.id }
@@ -62,26 +57,48 @@ RSpec.describe Volcanic::Authenticator::V1::Permission, :vcr do
   end
 
   describe 'Update' do
-    let(:permission_to_update) { permission.find_by_id(new_permission.id) }
-    let(:new_mock_name) { SecureRandom.hex(6) }
-    let(:new_mock_description) { 'new_mock_description' }
-    before do
-      permission_to_update.name = new_mock_name
-      permission_to_update.description = new_mock_description
-      permission_to_update.save
+    let(:new_name) { 'new-permission' }
+
+    context 'When required field is nil' do
+      before { new_permission.name = nil }
+      it { expect { new_permission.save }.to raise_error permission_error }
     end
-    context 'When updated' do
+
+    context 'When required field is empty' do
+      before { new_permission.name = '' }
+      it { expect { new_permission.save }.to raise_error permission_error }
+    end
+
+    context 'When changed name' do
+      before do
+        new_permission.name = new_name
+        new_permission.save
+      end
       subject { permission.find_by_id(new_permission.id) }
-      its(:name) { should_not be nil }
-      its(:description) { should eq new_mock_description }
+      its(:name) { should eq new_name }
+    end
+
+    context 'When update with existing name' do
+      before { new_permission.name = new_name }
+      it { expect { new_permission.save }.to raise_error permission_error }
     end
   end
 
   describe 'Delete' do
-    before { new_permission.delete }
+    let(:permission_id) { new_permission.id }
     context 'When deleted' do
-      subject { permission.find_by_id(new_permission.id) }
+      before { new_permission.delete }
+      subject { permission.find_by_id(permission_id) }
       its(:active?) { should be false }
+    end
+
+    context 'when service already been deleted' do
+      it { expect { new_permission.delete }.to raise_error permission_error }
+    end
+
+    context 'When invalid or non-exist id' do
+      it { expect { permission.new(id: 'wrong-id').delete }.to raise_error permission_error }
+      it { expect { permission.new(id: 123_456_789).delete }.to raise_error permission_error }
     end
   end
 end
