86bb65fa80871266387df465c148955e59b6c929
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-04 15:13:10 +0800

    removed all method and replace with find. simplify code

--- lib/volcanic/authenticator/v1/principal.rb
+++ lib/volcanic/authenticator/v1/principal.rb
@@ -7,19 +7,50 @@ module Volcanic::Authenticator
   module V1
     ##
     # Handle principal api
-    # method => :create, :all, :find_by_id, :update, :delete
+    # method => :create, :all, :find_by_id, :save, :delete
     # attr => :name, :dataset_id, :id
     class Principal
+      include Request
+      include Error
       # Principal end-point
-      PRINCIPAL_URL = 'api/v1/principal'
-      PRINCIPAL_UPDATE_URL = 'api/v1/principal/update'
+      PRINCIPAL_URL = 'api/v1/principals'
+      EXCEPTION = :raise_exception_principal
 
-      attr_reader :name, :dataset_id, :id
+      attr_reader :id, :login_attempts
+      attr_accessor :name, :dataset_id
 
-      def initialize(name = nil, dataset_id = nil, id = nil)
-        @name = name
-        @dataset_id = dataset_id
-        @id = id
+      def initialize(**opt)
+        @id = opt[:id]
+        @name = opt[:name]
+        @dataset_id = opt[:dataset_id]
+        @login_attempts = opt[:login_attempts]
+      end
+
+      #
+      # to update a principal.
+      #  eg.
+      #   principal = Principal.find_by_id(1)
+      #   principal.name = 'new principal name'
+      #   principal.dataset_id = 10
+      #   principal.save
+      #
+      def save
+        payload = { name: name, dataset_id: dataset_id }.to_json
+        perform_post_and_parse EXCEPTION, "#{PRINCIPAL_URL}/#{id}", payload
+      end
+
+      #
+      # to delete a principal
+      #  eg.
+      #  principal = Principal.find_by_id(1)
+      #  principal.delete
+      #
+      # OR
+      #
+      #   Principal.new(id: 1).delete
+      #
+      def delete
+        perform_delete_and_parse EXCEPTION, "#{PRINCIPAL_URL}/#{id}"
       end
 
       class << self
@@ -34,74 +65,67 @@ module Volcanic::Authenticator
         #  principal.dataset_id # => 1
         #  principal.id # => 1
         #
-        def create(name, dataset_id)
-          payload = { name: name, dataset_id: dataset_id }.to_json
-          res = perform_post_request PRINCIPAL_URL, payload
-          raise_exception_principal res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          new(parser['name'], parser['dataset_id'], parser['id'])
+        def create(name, dataset_id, roles = [], privileges = [])
+          payload = { name: name,
+                      dataset_id: dataset_id,
+                      roles: roles,
+                      privileges: privileges }.to_json
+          parsed = perform_post_and_parse EXCEPTION, PRINCIPAL_URL, payload
+          new(parsed.transform_keys(&:to_sym))
         end
 
         #
-        # to receive all principals.
+        # to find principal
         #
-        #  eg.
-        #   principals = Principal.all
-        #   principals[0].name # =>
-        #   principal.name # => 'principal-a'
-        #   principal.dataset_id # => 1
-        #   ...
+        # eg.
+        #  1) find principal by id
+        #   principal = Principal.find(id: 1)
         #
-        # note: authenticator service need to implement sort or pagination.
+        #  NOTE: all below cases return an array of principal object
         #
-        def all
-          res = perform_get_request "#{PRINCIPAL_URL}/all"
-          raise_exception_principal res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          parser.map do |p|
-            new(p['name'], p['dataset_id'], p['id'])
+        #  1) by default
+        #   principal = Principal.find
+        #   principal.size # => 10
+        #
+        #  2) find by key name
+        #   principal = Principal.find(key_name: 'volcanic')
+        #
+        #  3) find by key name and set page size
+        #   principal = Principal.find(key_name: 'volcanic', page_size: 5)
+        #   principal.size # => 5
+        #
+        #  4) find by key name, set page size and set page
+        #   principal = Principal.find(key_name: 'volcanic' page: 2, page_size: 5)
+        #   principal.size # => 5
+        #   principal[0].id # => 6
+        #
+        #  5) find by dataset_id
+        #   principal = Principal.find(dataset_id: 10)
+        #   principal[0].dataset_id # => 10
+        #
+        def find(id: nil, key_name: '', dataset_id: '', page: 1, page_size: 10)
+          return find_by_id(id) unless id.nil?
+
+          params = %W[query=#{key_name} dataset_id=#{dataset_id} page=#{page} page_size=#{page_size}]
+          url = "#{PRINCIPAL_URL}?#{params.join('&')}"
+          parsed = perform_get_and_parse EXCEPTION, url
+          parsed['data'].map do |data|
+            new(data.transform_keys(&:to_sym))
           end
         end
 
         #
-        # to find principal by given id
-        #
+        # find principal by specific id
         # eg.
-        #   principal = Principal.find_by_id(principal_id)
+        #   principal = Principal.find_by_id(1)
         #   principal.name # => 'principal-a'
-        #   principal.dataset_id # => 1
+        #   principal.dataset_id # => 10
         #   ...
         #
         def find_by_id(id)
-          res = perform_get_request "#{PRINCIPAL_URL}/#{id}"
-          raise_exception_principal res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          new(parser['name'], parser['dataset_id'], parser['id'])
-        end
-
-        #
-        # to update a principal.
-        #  eg.
-        #  # attributes need to be in hash value
-        #  attributes = { name: 'newPrincipalName' }
-        #  Principal.update(principal_id, attributes)
-        #
-        def update(id, attr)
-          raise PrincipalError, 'Attributes must be a hash type' unless attr.is_a?(Hash)
-
-          payload = attr.to_json
-          res = perform_post_request "#{PRINCIPAL_UPDATE_URL}/#{id}", payload
-          raise_exception_principal res unless res.success?
-        end
-
-        #
-        # to delete a principal
-        #  eg.
-        #  Principal.delete(principal_id)
-        #
-        def delete(id)
-          res = perform_delete_request "#{PRINCIPAL_URL}/#{id}"
-          raise_exception_principal res unless res.success?
+          url = "#{PRINCIPAL_URL}/#{id}"
+          parsed = perform_get_and_parse EXCEPTION, url
+          new(parsed.transform_keys(&:to_sym))
         end
       end
     end
