1e5b057109df74789bc189e158ae381576ee8fbd
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-08 17:45:48 +0800

    update privileges url, use splat operator at initialize. add allow?, allow! and disallow! method.

--- lib/volcanic/authenticator/v1/privilege.rb
+++ lib/volcanic/authenticator/v1/privilege.rb
@@ -7,21 +7,92 @@ module Volcanic::Authenticator
   module V1
     #
     # Privileges api
-    # method => :create, :all, find_by_id :update, :delete
-    # attr => :scope, :id, :permission_id, :group_id, :allow
     class Privilege
-      PRIVILEGE_URL = 'api/v1/privilege'
-      PRIVILEGES_URL = 'api/v1/privileges'
+      include Request
+      include Error
 
-      attr_reader :scope, :id, :permission_id, :group_id, :allow
-      ##
+      PRIVILEGE_URL = 'api/v1/privileges'
+
+      attr_reader :id, :subject_id
+      attr_accessor :scope, :permission_id, :group_id
+
+      #
       # initialize new privilege
-      def initialize(scope = nil, id = nil, permission_id = nil, group_id = nil, allow = nil)
-        @scope = scope
+      def initialize(id:, scope: nil, permission_id: nil, group_id: nil, **opts)
         @id = id
+        @scope = scope
         @permission_id = permission_id
         @group_id = group_id
-        @allow = allow == 1
+        @subject_id = opts[:subject_id]
+        @allow = opts[:allow]
+      end
+
+      #
+      # to check if privilege is allowed not not
+      # eg.
+      #   privilege = Privilege.find_by_id(1)
+      #   privilege.allow? # => true
+      #
+      def allow?
+        @allow == 1
+      end
+
+      #
+      # to allow a privilege
+      # eg.
+      #   privilege = Privilege.find_by_id(1)
+      #   privilege.allow!
+      #
+      #   prv = Privilege.find_by_id(1)
+      #   prv.allow? #=> true
+      #
+      def allow!
+        return if allow?
+
+        update(allow: 1)
+        @allow = 1
+      end
+
+      #
+      # to disallow a privilege
+      # eg.
+      #   privilege = Privilege.find_by_id(1)
+      #   privilege.disallow!
+      #
+      #   prv = Privilege.find_by_id(1)
+      #   prv.allow? #=> false
+      #
+      def disallow!
+        return unless allow?
+
+        update(allow: 0)
+        @allow = 0
+      end
+
+      #
+      # Update privilege
+      # eg.
+      #   privilege = Privilege.find_by_id(1)
+      #   privilege.scope = 'update scope'
+      #   privilege.permission_id = 2
+      #   privilege.group_id = 10
+      #   privilege.save
+      #
+      def save
+        update(scope: scope,
+               permission_id: permission_id,
+               group_id: group_id)
+      end
+
+      #
+      # Delete privilege
+      #  eg.
+      #   privilege = Privilege.find_by_id(1)
+      #   privilege.delete
+      #
+      def delete
+        res = perform_delete_request "#{PRIVILEGE_URL}/#{id}"
+        raise_exception_privilege res unless res.success?
       end
 
       class << self
@@ -36,83 +107,57 @@ module Volcanic::Authenticator
         #  privilege.permission_id # => 10
         #  privilege.group_id # => 2
         #
-        def create(scope, permission_id = nil, group_id = nil)
+        def create(scope, permission_id, group_id = nil)
           payload = { scope: scope,
                       permission_id: permission_id,
                       group_id: group_id }.to_json
           res = perform_post_request PRIVILEGE_URL, payload
           raise_exception_privilege res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          new(parser['scope'], parser['id'], parser['permission_id'], parser['group_id'])
+          parsed = JSON.parse(res.body)['response']
+          new(parsed.transform_keys(&:to_sym))
         end
 
         #
         # to receive an array of privileges
         #
         # eg.
-        #   privileges = privilege.all
+        #   privileges = privilege.find
         #   privileges[0].name # => 'privilege-a'
         #   privileges[0].id # => '<privilege_ID>'
         #   ....
         #
-        def all
-          res = perform_get_request PRIVILEGES_URL
+        def find(key_scope: '', page: 1, page_size: 10)
+          params = %W[query=#{key_scope} page=#{page} page_size#{page_size}]
+          url = "#{PRIVILEGE_URL}?#{params.join('&')}"
+          res = perform_get_request url
           raise_exception_privilege res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          parser.map do |privilege|
-            new(privilege['scope'],
-                privilege['id'],
-                privilege['permission_id'],
-                privilege['group_id'],
-                privilege['allow'])
-          end
+          parsed = JSON.parse(res.body)['response']
+          parsed['data'].map { |data| new(data.transform_keys(&:to_sym)) }
         end
 
         #
         # to find by given id
         #
         # eg.
-        #   privilege = privilege.find_by_id(privilege_id)
+        #   privilege = privilege.find_by_id(1)
         #   privilege.name # => 'privilege-a'
-        #   privilege.id # => '<privilege_ID>'
+        #   privilege.id # => 1
         #   ....
         #
         def find_by_id(id)
           res = perform_get_request "#{PRIVILEGE_URL}/#{id}"
           raise_exception_privilege res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          new(parser['scope'],
-              parser['id'],
-              parser['permission_id'],
-              parser['group_id'],
-              parser['allow'])
+          parsed = JSON.parse(res.body)['response']
+          new(parsed.transform_keys(&:to_sym))
         end
+      end
 
-        ##
-        # Update privilege
-        #  Eg.
-        #  attributes = { scope: 'this is new scope', permission_id: 10, group_id: 2 }
-        #  Privilege.update(privilege_id, attributes)
-        #
-        # attributes => :scope, :group_id, :permission_id
-        #
-        def update(id, attributes)
-          raise PrivilegeError, 'Attributes must be a hash type' unless attributes.is_a?(Hash)
+      # this is private method
+      private
 
-          payload = attributes.to_json
-          res = perform_post_request "#{PRIVILEGE_URL}/#{id}", payload
-          raise_exception_privilege res unless res.success?
-        end
-
-        ##
-        # Delete privilege
-        #  Eg.
-        #  privilege.delete(1)
-        #
-        def delete(id)
-          res = perform_delete_request "#{PRIVILEGE_URL}/#{id}"
-          raise_exception_privilege res unless res.success?
-        end
+      def update(**payload)
+        res = perform_post_request "#{PRIVILEGE_URL}/#{id}", payload.to_json
+        raise_exception_privilege res unless res.success?
       end
     end
   end
