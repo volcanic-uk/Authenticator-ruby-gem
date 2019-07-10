145022311d6a62714fa4b02405ae8aa59fe191bc
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-10 09:55:04 +0800

    refactor principal method. add active?, save, delete. apply splat operator to initialize

--- lib/volcanic/authenticator/v1/principal.rb
+++ lib/volcanic/authenticator/v1/principal.rb
@@ -10,15 +10,47 @@ module Volcanic::Authenticator
     # method => :create, :all, :find_by_id, :update, :delete
     # attr => :name, :dataset_id, :id
     class Principal
+      include Request
+      include Error
+
       # Principal end-point
       PRINCIPAL_URL = 'api/v1/principals'
+      EXCEPTION = :raise_exception_principal
 
-      attr_reader :name, :dataset_id, :id
+      attr_reader :id
+      attr_accessor :name, :dataset_id
 
-      def initialize(name = nil, dataset_id = nil, id = nil)
-        @name = name
-        @dataset_id = dataset_id
+      def initialize(id:, **opts)
         @id = id
+        @name = opts[:name]
+        @dataset_id = opts[:dataset_id]
+        @active = opts[:active]
+      end
+
+      #
+      # check activation
+      # eg .
+      #
+      def active?
+        @active == 1
+      end
+
+      #
+      # to update a principal.
+      #  eg.
+      #
+      def save
+        payload = { name: name, dataset_id: dataset_id }.to_json
+        perform_post_and_parse EXCEPTION, "#{PRINCIPAL_URL}/#{id}", payload
+      end
+
+      #
+      # to delete a principal
+      #  eg.
+      #  Principal.new(id: 1).delete
+      #
+      def delete
+        perform_delete_and_parse EXCEPTION, "#{PRINCIPAL_URL}/#{id}"
       end
 
       class << self
@@ -28,37 +60,29 @@ module Volcanic::Authenticator
         # to create a new Principal.
         #
         # eg.
-        #  principal = Volcanic::Authenticator::V1::Principal.create(principal_name, dataset_id)
+        #  principal = Volcanic::Authenticator::V1::Principal.create(principal_name, dataset_id, roles, privileges)
         #  principal.name # => 'principal-a'
         #  principal.dataset_id # => 1
         #  principal.id # => 1
         #
-        def create(name, dataset_id)
-          payload = { name: name, dataset_id: dataset_id }.to_json
-          res = perform_post_request PRINCIPAL_URL, payload
-          raise_exception_principal res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          new(parser['name'], parser['dataset_id'], parser['id'])
+        def create(name, dataset_id, roles = [], privileges = [])
+          payload = { name: name,
+                      dataset_id: dataset_id,
+                      roles: roles,
+                      privileges: privileges }.to_json
+          parsed = perform_post_and_parse EXCEPTION, PRINCIPAL_URL, payload
+          new(parsed.transform_keys(&:to_sym))
         end
 
         #
-        # to receive all principals.
+        # to find principals.
         #
-        #  eg.
-        #   principals = Principal.all
-        #   principals[0].name # =>
-        #   principal.name # => 'principal-a'
-        #   principal.dataset_id # => 1
-        #   ...
-        #
-        # note: authenticator service need to implement sort or pagination.
-        #
-        def all
-          res = perform_get_request "#{PRINCIPAL_URL}/all"
-          raise_exception_principal res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          parser.map do |p|
-            new(p['name'], p['dataset_id'], p['id'])
+        def find(key_name: '', page: 1, page_size: 10)
+          params = %W[query=#{key_name} page=#{page} page_size=#{page_size}]
+          url = "#{PRINCIPAL_URL}?#{params.join('&')}"
+          parsed = perform_get_and_parse EXCEPTION, url
+          parsed['data'].map do |data|
+            new(data.transform_keys(&:to_sym))
           end
         end
 
@@ -72,35 +96,10 @@ module Volcanic::Authenticator
         #   ...
         #
         def find_by_id(id)
-          res = perform_get_request "#{PRINCIPAL_URL}/#{id}"
-          raise_exception_principal res unless res.success?
-          parser = JSON.parse(res.body)['response']
-          new(parser['name'], parser['dataset_id'], parser['id'])
-        end
+          raise ArgumentError, 'id is empty or nil' if id.nil? || id == ''
 
-        #
-        # to update a principal.
-        #  eg.
-        #  # attributes need to be in hash value
-        #  attributes = { name: 'newPrincipalName' }
-        #  Principal.update(principal_id, attributes)
-        #
-        def update(id, attr)
-          raise PrincipalError, 'Attributes must be a hash type' unless attr.is_a?(Hash)
-
-          payload = attr.to_json
-          res = perform_post_request "#{PRINCIPAL_URL}/#{id}", payload
-          raise_exception_principal res unless res.success?
-        end
-
-        #
-        # to delete a principal
-        #  eg.
-        #  Principal.delete(principal_id)
-        #
-        def delete(id)
-          res = perform_delete_request "#{PRINCIPAL_URL}/#{id}"
-          raise_exception_principal res unless res.success?
+          parsed = perform_get_and_parse EXCEPTION, "#{PRINCIPAL_URL}/#{id}"
+          new(parsed.transform_keys(&:to_sym))
         end
       end
     end
