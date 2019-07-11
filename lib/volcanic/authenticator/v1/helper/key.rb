25fe0be354446b4e3296ec0904f417168ee97eb5
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-07-11 16:40:33 +0800

    removed kid null. kid always needed when requesting public key

--- lib/volcanic/authenticator/v1/helper/key.rb
+++ lib/volcanic/authenticator/v1/helper/key.rb
@@ -19,22 +19,19 @@ module Volcanic::Authenticator
         PUBLIC_KEY = 'volcanic_public_key'
         # Public Key end-point
         PUBLIC_KEY_URL = 'api/v1/key'
-        EXCEPTION = :raise_exception_standard
+        EXCEPTION = :raise_exception_app_token
 
-        def fetch_and_request(kid = nil)
-          cache.fetch(kid || PUBLIC_KEY, expire_in: exp_public_key) do
+        def fetch_and_request(kid)
+          cache.fetch PUBLIC_KEY, expire_in: exp_public_key do
             request_public_key(kid)
           end
         end
 
-        def request_public_key(kid = nil)
+        def request_public_key(kid)
           url = [PUBLIC_KEY_URL, kid].join('/')
-          # request an Application token for authorization header
           auth_token = AppToken.request_app_token
-          parsed = perform_get_and_parse EXCEPTION, "#{url}?expired=true", auth_token
-          # return a public key
-          # for dynamic => parsed['public_key']
-          OpenSSL::PKey.read(parsed)
+          parsed = perform_post_and_parse EXCEPTION, "#{url}?expired=true", nil, auth_token
+          OpenSSL::PKey.read(parsed['public_key'])
         rescue OpenSSL::PKey::PKeyError => e
           raise SignatureError, e
         end
