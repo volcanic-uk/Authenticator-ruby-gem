0f74ffd689daef0430fa7002d5c6d694a7efdf3a
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-08-02 17:56:49 +0800

    improve identity method

--- lib/volcanic/authenticator/v1/identity.rb
+++ lib/volcanic/authenticator/v1/identity.rb
@@ -2,34 +2,33 @@
 
 require_relative 'helper/error'
 require_relative 'helper/request'
-require_relative 'base'
 
 module Volcanic::Authenticator
   module V1
-    #
     # Handle identity api
-    # method => :register, :deactivate
-    # attr => :name, :secret, :id, :principal_id, :token
-    class Identity < Base
+    class Identity
       include Request
       include Error
 
       # end-point
-      IDENTITY_CREATE_URL = 'api/v1/identity'
+      URL = 'api/v1/identity'
       TOKEN_CREATE_URL = 'api/v1/identity/login'
       IDENTITY_DELETE_URL = 'api/v1/identity/deactivate'
+      RESET_SECRET_URL = 'api/v1/identity/secret/reset'
       EXCEPTION = :raise_exception_identity
 
-      attr_reader :id, :name, :secret, :principal_id
+      attr_accessor :name, :principal_id
+      attr_reader :id, :secret, :created_at, :updated_at
 
-      def initialize(id: nil, name: nil, secret: nil, principal_id: nil, **_opts)
+      def initialize(id:, name: nil, secret: nil, principal_id: nil, **opts)
         @id = id
         @name = name
         @principal_id = principal_id
         @secret = secret
+        @created_at = opts[:created_at]
+        @updated_at = opts[:updated_at]
       end
 
-      #
       # to generate new token.
       # this is similar to
       #   Token.create(name, secret).token
@@ -38,19 +37,37 @@ module Volcanic::Authenticator
       #  token = Identity.new(name, secret).token
       #  # => return a token
       #
-      def token
-        # Token.create(@name, @secret).token
-        payload = { name: name, secret: secret }.to_json
-        perform_post_and_parse(EXCEPTION, TOKEN_CREATE_URL, payload)['token']
-      end
+      # def token
+      #   # Token.create(name, secret)
+      # end
 
-      ##
       # Deactivate an identity and blacklist all associate tokens.
       # Eg.
       #   Identity.delete(identity_id)
       #
       def delete
         perform_post_and_parse EXCEPTION, "#{IDENTITY_DELETE_URL}/#{id}"
+      end
+
+      # updating identity
+      # eg.
+      #   identity = Identity.create(...)
+      #   identity.name = 'new name'
+      #   identity.principal_id = 1
+      #   identity.save
+      #
+      #   OR
+      #
+      #   Identity.new(id: 1, name: 'new name', principal_id: 1).save
+      def save
+        payload = { name: name, principal_id: principal_id }.to_json
+        perform_post_and_parse EXCEPTION, "#{URL}/#{id}", payload
+      end
+
+      def reset_secret(new_secret = nil)
+        payload = { secret: new_secret }.delete_if { |_, value| value.nil? }
+        parsed = perform_post_and_parse EXCEPTION, "#{RESET_SECRET_URL}/#{id}", payload.to_json
+        @secret = new_secret.nil? ? parsed['secret'] : new_secret
       end
 
       class << self
@@ -67,7 +84,7 @@ module Volcanic::Authenticator
                       secret: secret,
                       privileges: privileges,
                       roles: roles }.to_json
-          parsed = perform_post_and_parse EXCEPTION, IDENTITY_CREATE_URL, payload
+          parsed = perform_post_and_parse EXCEPTION, URL, payload
           parsed['secret'] = secret if parsed['secret'].nil?
           new(parsed.transform_keys!(&:to_sym))
         end
