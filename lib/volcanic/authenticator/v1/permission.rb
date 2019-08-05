b1b22c96d99fe2622b50cd087d385cfcb95b84ba
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-08-05 12:11:15 +0800

    add common class

--- lib/volcanic/authenticator/v1/permission.rb
+++ lib/volcanic/authenticator/v1/permission.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'helper/error'
-require_relative 'helper/request'
+require_relative 'common'
 
 module Volcanic::Authenticator
   module V1
-    #
     # Handle permission api
-    class Permission
-      include Request
-      include Error
-      # end-point
-      PERMISSION_URL = 'api/v1/permissions'
+    class Permission < Common
+      URL = 'api/v1/permissions'
       EXCEPTION = :raise_exception_permission
 
       attr_accessor :name, :description
@@ -29,122 +24,16 @@ module Volcanic::Authenticator
         @updated_at = opt[:updated_at]
       end
 
-      #
-      # to check for permission activation
-      # eg.
-      #   permission = Permission.find(1)
-      #   permission.active? # => true
-      #
-      def active?
-        @active.nil? ? Permission.find(id).active? : @active
-      end
-
-      #
-      # to update a permission
-      # eg.
-      #   permission = Permission.find(1)
-      #   permission.name = 'new permission name'
-      #   permission.description = 'new permission description'
-      #   permission.save
-      #
       def save
-        payload = { name: name, description: description }.to_json
-        perform_post_and_parse EXCEPTION, "#{PERMISSION_URL}/#{id}", payload
+        payload = { name: name, description: description }
+        super payload
       end
 
-      #
-      # to delete a permission
-      # eg.
-      #  Permission.new(id: 1).delete
-      #
-      # or
-      #
-      #  Permission.find(1).delete
-      #
-      def delete
-        perform_delete_and_parse EXCEPTION, "#{PERMISSION_URL}/#{id}"
-      end
-
-      class << self
-        include Request
-        include Error
-        #
-        # to _create a new permission.
-        #
-        # eg.
-        #  permission = Permission.create(permission_name, service_id, descriptions)
-        #  permission.name # => 'permission-a'
-        #  permission.id # => 1
-        #  permission.creator_id # => 1
-        #  ...
-        #
-        def create(name, service_id, description = nil)
-          payload = { name: name,
-                      description: description,
-                      service_id: service_id }.to_json
-          parsed = perform_post_and_parse EXCEPTION, PERMISSION_URL, payload
-          new(parsed.transform_keys(&:to_sym))
-        end
-
-        #
-        # find permission or permissions
-        #
-        def find(id = nil, **opts)
-          if id.nil?
-            opts[:page] ||= 1
-            opts[:page_size] ||= 10
-            find_with(opts)
-          else
-            find_by_id(id)
-          end
-        end
-
-        #
-        # get first object
-        #
-        def first
-          find(page_size: 1)[0]
-        end
-
-        #
-        # get last object
-        #
-        def last
-          find(page: count, page_size: 1)[0]
-        end
-
-        #
-        # get total count
-        #
-        def count
-          find(page_size: 1, pagination: true)[:pagination][:rowCount]
-        end
-
-        private
-
-        def find_with(pagination: false, **opts)
-          params = opts.map { |k, v| "#{k}=#{v}" }.join('&')
-          url = "#{PERMISSION_URL}?#{params}"
-          parsed = perform_get_and_parse EXCEPTION, url
-          data = parsed['data'].map { |d| new(d.transform_keys(&:to_sym)) }
-          if pagination
-            { pagination: parsed['pagination'].transform_keys(&:to_sym),
-              data: data }
-          else
-            data
-          end
-        end
-
-        #
-        # to find permission by given id
-        #
-        #
-        def find_by_id(id)
-          raise ArgumentError, 'id is empty or nil' if id.nil? || id == ''
-
-          parsed = perform_get_and_parse EXCEPTION, "#{PERMISSION_URL}/#{id}"
-          new(parsed.transform_keys(&:to_sym))
-        end
+      def self.create(name, service_id, description = nil)
+        payload = { name: name,
+                    description: description,
+                    service_id: service_id }
+        super payload
       end
     end
   end
