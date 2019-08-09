514640e5e0af0815aab3988b015eccfeae45e63e
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-08-09 12:58:33 +0800

    fix bug authorise

--- lib/volcanic/authenticator/v1/authorize.rb
+++ lib/volcanic/authenticator/v1/authorize.rb
@@ -18,39 +18,44 @@ module Volcanic::Authenticator
         url = "#{URL}/#{end_point}?fullyQualifiedSubject=#{subject}"
         exception = :raise_exception_privilege
         privileges = perform_get_and_parse(exception, url)
-        allow?(privileges, subject, permission, resource_id)
-      end
-
-      def allow?(privileges, subject, permission, id)
         return false if privileges.nil?
 
         return privileges.first['allow'] if privileges.count == 1
 
-        temp_allow = false
+        allow?(privileges, subject, permission, resource_id)
+      end
+
+      def allow?(privileges, subject, permission, id)
+        priorities = []
         subject = subject.split('/')
         privileges.each do |p|
           scope = p['scope'].split(':')
           stack = scope[1]
           dataset_id = scope[2]
           resource = scope.last.split('/')
-
+          priority = 0
           # check for accurate stack
           next unless ['*', subject[2]].include?(stack)
 
+          priority += 10 if stack == subject[2]
           # check for accurate dataset_id
           next unless ['*', subject[3]].include?(dataset_id)
 
+          priority += 30 if dataset_id == subject[3]
           # check for accurate permission/resource type
           next unless permission == resource.first
 
           # check for accurate resource id
           next unless ['*', id].include?(resource.last)
 
-          temp_allow = p['allow']
-          break unless temp_allow
-        end
+          priority += 50 if resource.last == id
+          priority += 1 unless p['allow']
 
-        temp_allow
+          priorities.push(priority: priority, allow: p['allow'])
+        end
+        return false if priorities.nil?
+
+        priorities.max_by { |a| a[:priority] }[:allow]
       end
     end
   end
