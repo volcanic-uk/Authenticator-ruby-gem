e7802f4daa758d0ce8d1e3dc086cba9fa6ba2eb5
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-08-14 10:18:53 +0800

    fix bug

--- lib/volcanic/authenticator/v1/authorize.rb
+++ lib/volcanic/authenticator/v1/authorize.rb
@@ -12,20 +12,20 @@ module Volcanic::Authenticator
 
       URL = 'api/v1/privileges'
 
-      def authorize_method(subject, service, permission, action, resource_id = '*')
+      def authorize_method(subject, service, permission, action, resource_id)
         # get privileges
         end_point = [service, "#{permission}:#{action}"].join('/')
         url = "#{URL}/#{end_point}?fullyQualifiedSubject=#{subject}"
         exception = :raise_exception_privilege
         privileges = perform_get_and_parse(exception, url)
-        return false if privileges.nil?
+        return false if privileges.empty?
 
         return privileges.first['allow'] if privileges.count == 1
 
         allow?(privileges, subject, permission, resource_id)
       end
 
-      def allow?(privileges, subject, permission, id)
+      def allow?(privileges, subject, permission, id = '*')
         priorities = []
         subject = subject.split('/')
         privileges.each do |p|
@@ -35,7 +35,7 @@ module Volcanic::Authenticator
           resource = scope.last.split('/')
           priority = 0
           # check for accurate stack
-          next unless ['*', subject[2]].include?(stack)
+          next unless ['*', subject[2]].include?(scope[1])
 
           priority += 10 if stack == subject[2]
           # check for accurate dataset_id
@@ -46,13 +46,13 @@ module Volcanic::Authenticator
           next unless permission == resource.first
 
           # check for accurate resource id
-          next unless ['*', id].include?(resource.last)
+          next unless ['*', id].include?(resource.last.to_i)
 
-          priority += 50 if resource.last == id
+          priority += 50 if resource.last.to_i == id
           priority += 1 unless p['allow']
-
           priorities.push(priority: priority, allow: p['allow'])
         end
+
         return false if priorities.nil?
 
         priorities.max_by { |a| a[:priority] }[:allow]
