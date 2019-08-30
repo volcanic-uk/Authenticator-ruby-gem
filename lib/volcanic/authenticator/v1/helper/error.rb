bc82970707d36d290a419e88eb18c1ad1d984b41
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-08-30 12:31:05 +0800

    improve error helper

--- lib/volcanic/authenticator/v1/helper/error.rb
+++ lib/volcanic/authenticator/v1/helper/error.rb
@@ -4,36 +4,51 @@ module Volcanic::Authenticator
   module V1
     # Error helper
     module Error
+      # raise exception
+      class RaiseException
+        attr_reader :res, :status, :body, :exception
+
+        def initialize(res, exception)
+          @res = res
+          @status = res.code
+          @body = JSON.parse(res.body)
+          @exception = exception
+          raise_exception
+        end
+
+        private
+
+        def raise_exception
+          standard_error
+          raise exception, message if [400, 404, 422].include?(status)
+        end
+
+        def standard_error
+          raise SignatureError, message if status == 400 && error_code == 3002
+          raise AuthorizationError, message if [401, 403].include?(status)
+        end
+
+        def message
+          return "HTTP error #{status}" unless body.key?('message')
+
+          body['message']
+        end
+
+        def error_code
+          return '' unless body.key?('errorCode')
+
+          body['errorCode']
+        end
+      end
+
       # error handler for application token
       def raise_exception_app_token(res)
-        code = res.code
-        body = res.body
-        raise_exception_standard(res)
-        raise ApplicationTokenError, parser(body, 'message') if code == 400
+        RaiseException.new(res, ApplicationTokenError)
       end
 
       # error handler for service
       def raise_exception_service(res)
-        code = res.code
-        body = res.body
-        raise_exception_standard(res)
-        raise ServiceError, parser(body, 'message') if code == 400
-        raise ServiceError if code == 404
-      end
-
-      # default error handler
-      def raise_exception_standard(res)
-        code = res.code
-        body = res.body
-        raise SignatureError, parser(body, 'message') if code == 400 && parser(body, 'errorCode' == 3002)
-        raise AuthorizationError, parser(body, 'message') if [401, 403].include?(code)
-      end
-
-      def parser(json, *key)
-        keys = key.flatten.compact
-        keys.reduce(JSON.parse(json)) { |found, item| found[item] }
-      rescue TypeError
-        raise ArgumentError, 'JSON key not found.'
+        RaiseException.new(res, ServiceError)
       end
     end
   end
