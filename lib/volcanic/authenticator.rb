ef65d080859e173e4021adda442ac2dc315f08b3
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-10-09 14:34:03 +0800

    fix bug

--- lib/volcanic/authenticator.rb
+++ lib/volcanic/authenticator.rb
@@ -14,7 +14,6 @@ require_relative 'authenticator/v1/principal.rb'
 require_relative 'authenticator/v1/identity.rb'
 require_relative 'authenticator/v1/token.rb'
 require_relative 'authenticator/v1/resource.rb'
-require_relative 'authenticator/v1/service_request'
 require_relative 'authenticator/v1/http_request'
 
 module Volcanic
