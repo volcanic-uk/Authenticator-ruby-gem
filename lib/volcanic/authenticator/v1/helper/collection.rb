8498b460fbb4965e97d4d7ea6e1d6bf71a1dddc3
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-10-10 17:39:07 +0800

    update snake case handler. move to collection.rb

--- lib/volcanic/authenticator/v1/helper/collection.rb
+++ lib/volcanic/authenticator/v1/helper/collection.rb
@@ -5,6 +5,8 @@ module Volcanic::Authenticator
     # collection helper
     # this class extend array class with a few custom gettter and setter
     class Collection < Array
+      KEYS_FROM_AUTH_SERVICE = { pageSize: :page_size, rowCount: :row_count, pageCount: :page_count }.freeze
+
       attr_accessor :page, :page_size, :row_count, :page_count
 
       def initialize(array, page: nil, page_size: nil, row_count: nil, page_count: nil, **_opts)
@@ -13,6 +15,14 @@ module Volcanic::Authenticator
         @row_count = row_count
         @page_count = page_count
         super array
+      end
+
+      # converting camel case to a snake case style
+      def self.from_auth_service(array, **args)
+        KEYS_FROM_AUTH_SERVICE.each do |svc_key, ruby_key|
+          args[ruby_key] = args.delete(svc_key) if args.key?(svc_key)
+        end
+        new(array, **args)
       end
     end
   end
