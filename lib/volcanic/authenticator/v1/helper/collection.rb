6c1e009aac8f6527d0ef6a5ef8c6d02c4e39719b
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-10-10 16:51:53 +0800

    add snake_case convertor

--- lib/volcanic/authenticator/v1/helper/collection.rb
+++ lib/volcanic/authenticator/v1/helper/collection.rb
@@ -7,17 +7,13 @@ module Volcanic::Authenticator
     class Collection < Array
       attr_accessor :page, :page_size, :row_count, :page_count
 
-      # rubocop:disable Naming/UncommunicativeMethodParamName
-      # rubocop:disable Naming/VariableName:
-      def initialize(array, page: nil, pageSize: nil, rowCount: nil, pageCount: nil, **_opts)
+      def initialize(array, page: nil, page_size: nil, row_count: nil, page_count: nil, **_opts)
         @page = page
-        @page_size = pageSize
-        @row_count = rowCount
-        @page_count = pageCount
+        @page_size = page_size
+        @row_count = row_count
+        @page_count = page_count
         super array
       end
-      # rubocop:enable Naming/UncommunicativeMethodParamName
-      # rubocop:enable Naming/VariableName:
     end
   end
 end
