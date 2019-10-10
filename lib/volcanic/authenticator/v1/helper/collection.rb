42b9853d6e5ffeffe283e9d9819edb7923d4b2b3
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-10-10 11:45:48 +0800

    remove unused method and improve collection class

--- lib/volcanic/authenticator/v1/helper/collection.rb
+++ lib/volcanic/authenticator/v1/helper/collection.rb
@@ -7,11 +7,11 @@ module Volcanic::Authenticator
     class Collection < Array
       attr_accessor :page, :page_size, :row_count, :page_count
 
-      def initialize(array, **page_information)
-        @page = page_information[:page]
-        @page_size =  page_information[:pageSize]
-        @row_count =  page_information[:rowCount]
-        @page_count = page_information[:pageCount]
+      def initialize(array, page: nil, pageSize: nil, rowCount: nil, pageCount: nil, **opts)
+        @page = page
+        @page_size = pageSize
+        @row_count = rowCount
+        @page_count = pageCount
         super array
       end
     end
