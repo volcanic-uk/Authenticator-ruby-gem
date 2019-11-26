3247436069627b651d2e2ed95a614f40c9e9dbc4
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-11-26 12:06:38 +0800

    AUTH-201 refactor warden-strategies and remove unuse config methods

--- lib/volcanic/authenticator/v1/warden/auth_failure.rb
+++ lib/volcanic/authenticator/v1/warden/auth_failure.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
 module Volcanic::Authenticator
-  module V1
-    module Warden
-      # this class create a rack for auth strategy failure
-      class AuthFailure
-        def call(env)
-          @env = env
-          [status, headers, [body]]
-        end
+  module V1::Warden
+    # this class create a rack for auth strategy failure
+    class AuthFailure
+      def call(env)
+        @env = env
+        [status, headers, [body]]
+      end
 
-        # http response
-        def status
-          401 # Unauthorized
-        end
+      # http response
+      def status
+        401 # Unauthorized
+      end
 
-        def headers
-          { 'Content-Type' => 'application/json' }
-        end
+      def headers
+        { 'Content-Type' => 'application/json' }
+      end
 
-        def body
-          { message: @env['warden'].message }.to_json
-        end
+      def body
+        { message: @env['warden'].message }.to_json
       end
     end
   end
