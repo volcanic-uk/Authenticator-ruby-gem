40b0ebb8e798d4dbb721ae2c93b4420f2092d8af
Dan Leyden <dan@52degreesnorth.com>
2020-03-05 16:27:17 +0000

    refactoring request

--- lib/volcanic/authenticator/v1/helper/request.rb
+++ lib/volcanic/authenticator/v1/helper/request.rb
@@ -16,32 +16,29 @@ module Volcanic::Authenticator
         base.extend self
       end
 
-      # performing post method request
-      def perform_post_and_parse(exception, end_point, body = nil, auth_token = AppToken.fetch_and_request)
+      def perform_request_and_parse(verb, exception, end_point, body = nil, auth_token = :undefined)
+        if auth_token == :undefined
+                           auth_token = AppToken.fetch_and_request
+                         end
         url = [Volcanic::Authenticator.config.auth_url, end_point].join('/')
-        res = HTTParty.post url, body: body, headers: bearer_header(auth_token)
+        res = HTTParty.send verb, url, body: body, headers: bearer_header(auth_token)
         exception_handler_and_parser(exception, res)
       rescue Timeout::Error, Errno::EINVAL, Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError => e
         raise ConnectionError, e
+      end
+
+      def perform_post_and_parse(exception, end_point, body = nil, auth_token = :undefined)
+        perform_request_and_parse(:post, exception, end_point, body, auth_token)
       end
 
       # performing get method request
-      def perform_get_and_parse(exception, end_point, auth_token = AppToken.fetch_and_request)
-        url = [Volcanic::Authenticator.config.auth_url, end_point].join('/')
-        res = HTTParty.get url, headers: bearer_header(auth_token)
-        exception_handler_and_parser(exception, res)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError => e
-        raise ConnectionError, e
+      def perform_get_and_parse(exception, end_point, auth_token = :undefined)
+        perform_request_and_parse(:get, exception, end_point, nil, auth_token)
       end
 
       # performing delete method request
-      def perform_delete_and_parse(exception, end_point)
-        url = [Volcanic::Authenticator.config.auth_url, end_point].join('/')
-        auth_token = AppToken.fetch_and_request
-        res = HTTParty.delete url, headers: bearer_header(auth_token)
-        exception_handler_and_parser(exception, res)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError => e
-        raise ConnectionError, e
+      def perform_delete_and_parse(exception, end_point, auth_token = :undefined)
+        perform_request_and_parse(:delete, exception, end_point, nil, auth_token)
       end
 
       def exception_handler_and_parser(exception, res)
